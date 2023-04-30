4e5c53d9dc16b963ae5ed51ded4c0f155c9a02bb
Alf-caput <alfre.roble.aba@gmail.com>
2023-04-30 20:30:06 +0200

    facial capture

--- preprocessing.py
+++ preprocessing.py
@@ -8,12 +8,16 @@ def main():
     face_detector = cv2.CascadeClassifier(r'haar_tools/haarcascade_frontalface_default.xml')
     mouth_detector = cv2.CascadeClassifier(r'haar_tools/mouth.xml')
     file_path = 0
+    facial_capture(face_detector, mouth_detector, file_path)
+    return 0
+
+
+def facial_capture(face_detector, mouth_detector, file_path=0):
     capture = cv2.VideoCapture(file_path)
     while capture.isOpened() and cv2.waitKey(1) not in (ord('s'), ord('S')):  # While 's' or 'S' not pressed
         read_successfully, main_frame = capture.read()
         if read_successfully:
             face_frame = get_face_frame(main_frame, face_detector)
-            print(face_frame)
             if face_frame is not None:
                 print("face found")
                 mouth_frame = get_mouth_frame(face_frame, mouth_detector)
